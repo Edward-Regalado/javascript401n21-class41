769c2938331ccbb5a07591fdae788cf4a9ebadc1
Edward-Regalado <tregalado30@gmail.com>
2022-12-03 02:30:07 +0000

    added api connection, images, icons

---
+++ myFirstApp/utils/getIconForWeather.js
@@ -0,0 +1,10 @@
+const icons = {
+    Clear: '☀️',
+    Clouds: '☁️',
+    Drizzle: '🌧️',
+    Rain: '⛈️',
+    Snow: '❄️',
+    Thunderstorm: '⛈️'
+  };
+  
+  export default weather => icons[weather];
