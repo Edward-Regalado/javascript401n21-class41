627b9ec9eb977d0bc518c9867a783d36e78bbd59
Edward-Regalado <tregalado30@gmail.com>
2022-12-06 02:45:16 +0000

    added Navigation container, Stack navigator and stack screen, added details page and wired up navigation

--- myFirstApp/App.js
+++ myFirstApp/App.js
@@ -1,12 +1,80 @@
 // import  { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Weather from './src/index';
+
+import React, {useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/home';
+import Details from './src/details';
+
+const Stack = createNativeStackNavigator();
 
 export default function App(){
-  return (
-    <View style={styles.container}>
-      <Weather />
-    </View>
+
+  const [forecast, setForecast] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
+  // const [weather, setWeather] = useState('');
+  // const [weather, setWeather] = useState('Clear');
+  const [weather, setWeather] = useState('Snow');
+  // const [weather, setWeather] = useState('Rain');
+  // const [weather, setWeather] = useState('Drizzle');
+  // const [weather, setWeather] = useState('Snow');
+  // const [weather, setWeather] = useState('Clouds');
+
+  // const loadForecast = async () => {
+  //   setRefreshing(true);
+
+  //   const { status } = await Location.requestForegroundPermissionsAsync();
+  //   if (status !== 'granted') {
+  //     Alert.alert('Permission to access location was denied');
+  //   }
+
+  //   let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+
+  //   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${OPEN_WEATHER_API}&units=imperial`);
+
+  //   const data = await response.json();
+
+  //   if(!response.ok) {
+  //     Alert.alert(`Error retrieving weather data: ${data.message}`); 
+  //   } else {
+  //     setForecast(data);
+  //       setWeather(data.weather[0].main);
+  //   }
+
+  //   setRefreshing(false);
+  // }
+
+  // useEffect(() => { 
+  //   if (!forecast) {
+  //     loadForecast(); 
+  //   }
+  // })
+
+  // if (!forecast) {
+  //   return <SafeAreaView style={styles.loading}>
+  //     <ActivityIndicator size="large" />
+  //     </SafeAreaView>;
+  // }
+
+  // const current = forecast.weather[0];
+  
+  return ( 
+   <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='Home'
+          component={Home}
+          initialParams={{weather: weather, forecast: forecast}}
+          // options={{ title:  'Welcome'}}
+        />
+        <Stack.Screen 
+          name='Details'
+          component={Details}
+          initialParams={{weather: weather, forecast: forecast}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- myFirstApp/README.md
+++ myFirstApp/README.md
@@ -32,3 +32,24 @@
 - Builder X
 - Properly Documented for End Users
 - Live Demo & Presentation
+
+
+## LAB - React Native - Milestone 2: Polish and Delivery
+
+### Application Requirements
+
+- Use 2 screens / components
+- Use and share state between components/screens
+- Use at least 1 native device feature (Contacts, Camera, GPS, etc)
+- Properly Documented for End Users
+- Live Demo & Presentation
+
+### Milestone 2 - Product Delivery (This Lab)
+
+As you complete your RNA, switch your focus to the user experience.
+
+- Quality and Thoughtful Styling using css-in-js
+- Right sized tap targets, good contrast
+- Dark and Light Modes
+- Well thought through UX
+- Be considerate of individuals that primarily use their thumbs, left or right hands
